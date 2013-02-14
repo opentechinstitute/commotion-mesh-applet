f9729220b11be9da7bd4f96d5593a36071fb5c20
Hans-Christoph Steiner <hans@eds.org>
2013-02-14 16:39:48 -0500

    template for GNOME 2.x/3.x compatible panel applet
    generated by:
    http://www.lshift.net/blog/2012/02/29/making-panel-applets-for-gnome-2-and-3
    https://github.com/palfrey/panel-applet-generator

---
+++ nm-dispatcher-olsrd-applet-factory2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import gtk
+import pygtk
+pygtk.require('2.0')
+import gnomeapplet
+from nm-dispatcher-olsrd-appletApplet import applet_factory
+
+if __name__ == '__main__':	 # testing for execution
+	print('Starting factory')
+
+	if len(sys.argv) > 1 and sys.argv[1] == '-d': # debugging
+		mainWindow = gtk.Window()
+		mainWindow.set_title('Applet window')
+		mainWindow.connect('destroy', gtk.main_quit)
+		applet = gnomeapplet.Applet()
+		applet_factory(applet, None)
+		applet.reparent(mainWindow)
+		mainWindow.show_all()
+		gtk.main()
+		sys.exit()
+	else:
+		gnomeapplet.bonobo_factory('OAFIID:nm-dispatcher-olsrd-applet_Factory',
+				gnomeapplet.Applet.__gtype__,
+				'nm-dispatcher-olsrd-applet',
+				'0.0',
+				applet_factory)
